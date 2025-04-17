53a4c1b92c85b6f2e091d0b015568a96c1a17a01
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-04-17 10:30:03 -0700

    updating dataset for KS

--- data/data_bfs_preprocess.py
+++ data/data_bfs_preprocess.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 class bfs_dataset(Dataset):
 	def __init__(self,
-				 data_location=['./data0.npy', './data1.npy'],
+				 data_location=['./data0.npy'],
 				 trajec_max_len=50,
 				 start_n=0,
 				 n_span=510):
@@ -18,10 +18,10 @@ class bfs_dataset(Dataset):
 		self.trajec_max_len = trajec_max_len
 
 		solution0 = np.load(data_location[0],allow_pickle = True)
-		solution1 = np.load(data_location[1],allow_pickle = True)
-		solution  = np.concatenate([solution0,
-									solution1],axis = 0)
-		self.solution = torch.from_numpy(solution[start_n:start_n+n_span])
+		#solution1 = np.load(data_location[1],allow_pickle = True)
+		#solution  = np.concatenate([solution0,
+		#							solution1],axis = 0)
+		self.solution = torch.from_numpy(solution0[start_n:start_n+n_span])
 
 	def __len__(self):
 		return self.n_span - self.trajec_max_len
