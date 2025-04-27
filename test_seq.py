d9c560ae645d3df08d57e4e556bb9ba845205319
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-04-27 14:01:01 -0700

    testing other sequential model

--- test_seq.py
+++ test_seq.py
@@ -391,8 +391,8 @@ class Args_eval:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Evaluate Sequential Model')
         # Model/Training Args Location
-        self.parser.add_argument("--train_args_txt", type=str, default='./output/ks_2025_04_26_00_56_49/logging/args.txt', help='Path to args.txt from training')
-        self.parser.add_argument("--model_path", type=str,default='./output/ks_2025_04_26_00_56_49/model_save/final_model_Nt_4000.pt', help='Path to the trained model .pt file')
+        self.parser.add_argument("--train_args_txt", type=str, default='./output/ks_2025_04_20_18_06_37/logging/args.txt', help='Path to args.txt from training')
+        self.parser.add_argument("--model_path", type=str,default='./output/ks_2025_04_20_18_06_37/model_save/final_model_Nt_2000.pt', help='Path to the trained model .pt file')
         # Data Args
         self.parser.add_argument("--data_location", type=str, default=['./data/data0.npy','./data/data1.npy'], help='Directory containing evaluation data (e.g., .npy files)')
         #self.parser.add_argument("--file_name", type=str, default='data_test.npy', help='Name of the evaluation data file')
