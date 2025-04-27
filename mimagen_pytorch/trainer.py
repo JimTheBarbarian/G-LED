bc1f97fc9ce03a26b54471477d534ffed8084c9f
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-04-27 01:31:07 -0700

    oops

--- mimagen_pytorch/trainer.py
+++ mimagen_pytorch/trainer.py
@@ -999,7 +999,7 @@ class ImagenTrainer(nn.Module):
         #        loss = self.imagen(*chunked_args, unet = self.unet_being_trained, unet_number = unet_number, **chunked_kwargs)
         #        loss = loss * chunk_size_frac
 
-        with autocast(enabled = self.grad_scaler):
+        with autocast(enabled = self.grad_scaler_enabled):
             loss = self.imagen(*args, unet = self.unet_being_trained, unet_number = unet_number, **kwargs)
         total_loss += loss.item()
 
