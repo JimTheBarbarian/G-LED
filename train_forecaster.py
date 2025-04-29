c21ff7664ccef8193b07f5b6c252fd5535ce24a6
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-04-28 23:43:01 -0700

    changed the random seed

--- train_forecaster.py
+++ train_forecaster.py
@@ -270,7 +270,7 @@ def main():
     parser.add_argument('--scheduler_gamma', type=float, default=0.9, help='StepLR gamma')
     parser.add_argument('--output_dir', type=str, default='./forecasting_output', help='Directory to save results')
     # local_rank is handled by torchrun/launch
-    parser.add_argument('--seed', type=int, default=42, help='Random seed')
+    parser.add_argument('--seed', type=int, default=20398, help='Random seed')
 
 
     args = parser.parse_args()
